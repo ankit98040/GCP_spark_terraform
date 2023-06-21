fe3d2a18b44ff6240ab75c409af9271b7c53fe06
Ankit Pramanik <90780863+ankit-p090@users.noreply.github.com>
2023-06-21 12:05:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ locals {
 lab_prefix                  = "dll"
 project_id                  = "${var.project_id}"
 location                    = "${var.location}"
+umsa                        = "${local.lab_prefix}-lab-sa"
+umsa_fqn                    = "${local.umsa}@${local.project_id}.iam.gserviceaccount.com"
+admin_upn_fqn               = "${var.gcp_account_name}"
 }
