bd1bb8d902fc62c233248442ac7e922628956525
Ankit Pramanik <90780863+ankit-p090@users.noreply.github.com>
2023-06-21 12:47:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,10 @@ location                    = "${var.location}"
 umsa                        = "${local.lab_prefix}-lab-sa"
 umsa_fqn                    = "${local.umsa}@${local.project_id}.iam.gserviceaccount.com"
 admin_upn_fqn               = "${var.gcp_account_name}"
+vpc_nm                      = "${local.lab_prefix}-vpc"
+subnet_nm                   = "spark-snet"
+nat_nm                      = "${local.lab_prefix}-nat"
+nat_router_nm               = "${local.lab_prefix}-nat-router"
+subnet_cidr                 = "10.0.0.0/16"
+psa_ip_length               = 16
 }
