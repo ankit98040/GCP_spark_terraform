80c2bf9283fecc085a2b40dc0466a2c7ed661890
Ankit Pramanik <90780863+ankit-p090@users.noreply.github.com>
2023-07-11 18:28:03 +0530

    Update google_service_account.tf

--- 4/modules/Service_account_roles/google_service_account.tf
+++ 4/modules/Service_account_roles/google_service_account.tf
@@ -5,3 +5,15 @@ resource "google_service_account" "this" {
   account_id   = var.account_id
   display_name = var.display_name
 }
+
+
+# resource "google_service_account" "this" {
+#   account_id = var.account_id
+#   display_name = "SA"
+# }
+
+# resource "google_project_iam_member" "firestore_owner_binding" {
+#   project = var.project_id
+#   role    = "roles/composer.ServiceAgentV2Ex"
+#   member  = "serviceAccount:${google_service_account.this.email}"
+# }
